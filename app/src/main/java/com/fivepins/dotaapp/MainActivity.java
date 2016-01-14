02c20bb6bd2a1606e067cf132cdc7d0a7843c0ef
Svetla <sv.georgieva@gmail.com>
2016-01-15 00:19:16 +0200

    add pull to refresh on main activity listview

--- app/src/main/java/com/fivepins/dotaapp/MainActivity.java
+++ app/src/main/java/com/fivepins/dotaapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.fivepins.dotaapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -19,6 +21,8 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
 
     JSONArray allMatches = null;
+    private SwipeRefreshLayout swipeContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +34,18 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        // Construct the data source
-        ArrayList<Match> arrayOfMatches = new ArrayList<>();
+        final Context context = this;
 
         //URL to get JSON Array
         //Contains an array of allMatches objects
-        String url = getString(R.string.matches_json_url);
+        final String url = getString(R.string.matches_json_url);
+
+        // TODO make this one method a
+        // Construct the data source
+        ArrayList<Match> arrayOfMatches = new ArrayList<>();
 
         // Create and set Adapter for the ListView.
-        MatchAdapter adapter = new MatchAdapter(this, arrayOfMatches);
+        MatchAdapter adapter = new MatchAdapter(context, arrayOfMatches);
         ListView listView = (ListView) findViewById(R.id.match_list_view);
         listView.setAdapter(adapter);
 
@@ -46,6 +53,31 @@ public class MainActivity extends AppCompatActivity {
         // Load data asynchronously.
         LoadFeedData loadFeedData = new LoadFeedData(adapter);
         loadFeedData.execute(url);
+
+
+        // Swipe refresh part
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Load data asynchronously.
+                // TODO make this one method a
+                ArrayList<Match> arrayOfMatches2 = new ArrayList<>();
+                MatchAdapter adapter = new MatchAdapter(context, arrayOfMatches2);
+                LoadFeedData loadFeedData = new LoadFeedData(adapter);
+                loadFeedData.execute(url);
+                // Stop the refresh animation
+                swipeContainer.setRefreshing(false);
+            }
+        });
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 
 
